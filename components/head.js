97bb695d819fb40f08649819205cbedecc184f92
edward <edward@pond5.com>
2018-06-20 13:14:46 +0200

    Semantic: Include SUI css, add associated loaders, generate css file

--- components/head.js
+++ components/head.js
@@ -1,13 +1,14 @@
 import NextHead from 'next/head'
 import { string } from 'prop-types'
+import '../semantic/dist/semantic.min.css';
 
-const defaultDescription = ''
-const defaultOGURL = ''
-const defaultOGImage = ''
+const defaultDescription = '';
+const defaultOGURL = '';
+const defaultOGImage = '';
 
 const Head = (props) => (
   <NextHead>
-    <meta charset="UTF-8" />
+    <meta charSet="UTF-8" />
     <title>{props.title || ''}</title>
     <meta name="description" content={props.description || defaultDescription} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -24,14 +25,15 @@ const Head = (props) => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
+    <link rel="stylesheet" type="text/css" href={`/_next/static/styles.css`} />
   </NextHead>
-)
+);
 
 Head.propTypes = {
   title: string,
   description: string,
   url: string,
   ogImage: string
-}
+};
 
 export default Head
--- next.config.js
+++ next.config.js
@@ -1,10 +1,37 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
-    }
+    };
 
-    return config
+    config.module.rules.push({
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
+    });
+
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|eot)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {}
+        }
+      ]
+    });
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: "css-loader"
+      })
+    });
+
+    config.plugins.push(new ExtractTextPlugin("static/styles.css"));
+
+    return config;
   }
-}
+};
