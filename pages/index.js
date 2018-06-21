a84be62f0b1b19d657f976b0a0751c25e7fc749a
edward <edward@pond5.com>
2018-06-21 13:28:52 +0200

    Eslint: Lint Head, Layout and index components

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,22 @@
-import Head from '../components/head'
+import React from 'react';
 
 export default () => (
   <div>
-    <Head title="Home" />
-
     <div className="hero">
       <h1 className="title">Welcome to Bookshop</h1>
     </div>
-
     <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-    `}</style>
+    .hero {
+      width: 100%;
+      color: #333;
+    }
+    .title {
+      margin: 0;
+      width: 100%;
+      padding-top: 80px;
+      line-height: 1.15;
+      font-size: 48px;
+    }
+  `}</style>
   </div>
-)
+);
