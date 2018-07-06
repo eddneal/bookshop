5f272abd2fd51e7d1a17fd71082f48f4931a38c1
edward <edward@pond5.com>
2018-07-06 15:33:50 +0200

    Google API: Add Google Books API handler

---
+++ api/googleBooksHandler.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+export default (searchTerm = 'harry+potter') => {
+  return new Promise((resolve, reject) => {
+    request(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`, (err, resp, body) => {
+      if (err) return reject(err);
+      if (resp.statusCode === 200) {
+        const data = JSON.parse(body);
+        if ('items' in data) return resolve(data);
+        return reject(new Error('No books found'));
+      }
+    });
+  });
+};
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/Layout';
+import bookSearch from '../api/googleBooksHandler';
 
-export default () => (
-  <Layout page="Home">
-    <div className="hero">
-      <h1 className="title">Welcome to Bookshop</h1>
-    </div>
-    <style jsx>{`
-    .hero {
-      width: 100%;
-      color: #333;
-    }
-    .title {
-      margin: 0;
-      width: 100%;
-      padding-top: 80px;
-      line-height: 1.15;
-      font-size: 48px;
-    }
-  `}</style>
-  </Layout>
-);
+export default class extends Component {
+  static async getInitialProps() {
+    return bookSearch()
+      .then(response => response)
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <Layout page="Home">
+        <div className="hero">
+          <h1 className="title">Welcome to Bookshop</h1>
+        </div>
+        <style jsx>{`
+            .hero {
+            width: 100%;
+            color: #333;
+          }
+          .title {
+            margin: 0;
+            width: 100%;
+            padding-top: 80px;
+            line-height: 1.15;
+            font-size: 48px;
+          }
+        `}
+        </style>
+      </Layout>
+    );
+  }
+}
