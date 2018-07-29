fe70d2d76da06074d6d5464f99032804a2339607
edward <edward@pond5.com>
2018-07-29 15:47:43 +0200

    SearchResults: Add grid styles

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -3,11 +3,36 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const SearchResults = props => (
-  <ul>
+  <ul className="searchResults">
     {props.items && props.items.length && props.items.map(item => (
-      <li key={item.id}>{JSON.stringify(item.volumeInfo.title)}</li>
+      <li className="searchResult" key={item.id}>
+        <img className="bookCover" src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} />
+        <div className="bookTitle">{item.volumeInfo.title}</div>
+      </li>
     ))
     }
+    <style jsx>{`
+      .searchResults {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        grid-gap: 30px;
+      }
+      .searchResult {
+        display: flex;
+        flex-direction: column;
+      }
+      .bookCover {
+        width: 100%;
+        height: auto;
+      }
+      .bookTitle {
+        margin-top: auto;
+      }
+    `}
+    </style>
   </ul>
 );
 
