8c5b3de7221c39de651787c4be3f8df284910d5f
edward <edward@pond5.com>
2019-05-13 17:03:21 +0200

    HEADER: Credentials

--- authCredentials/client.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: 'AIzaSyDwDpp6e8uhQXo1YxBEqrjgTj0Lbt53ARI',
-  authDomain: 'bookshop-209407.firebaseapp.com',
-  databaseURL: 'https://bookshop-209407.firebaseio.com',
-  projectId: 'bookshop-209407',
-  storageBucket: 'bookshop-209407.appspot.com',
-  messagingSenderId: '129367984992',
-  appId: '1:129367984992:web:c8cc73a72e5fd400',
-};
