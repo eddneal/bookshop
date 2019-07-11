a62ef887da63a61bf11d0e2e3370188cfe36e340
edward <edward@pond5.com>
2019-07-11 17:44:06 +0200

    SEARCH: Switch to a functional component

--- pages/search.js
+++ pages/search.js
@@ -17,33 +17,17 @@ import {
   clearSearch,
 } from '../store/actions/search';
 
-class Search extends Component {
-  static async getInitialProps({ store, req, res, query }) {
-    const typeCastQuery = shallowParseInts(stateDefaults, query);
-    const { keyword, perPage, orderBy, filter, startIndex } = Object.assign(stateDefaults, typeCastQuery);
+function Search() {
+  const SearchInputWithSearchRoute = withSearchRoute(SearchInput);
+  const PerPageDropdownWithSearchRoute = withSearchRoute(PerPageDropdown);
+  const OrderByDropdownWithSearchRoute = withSearchRoute(OrderByDropdown);
+  const SearchInfoWithSearchRoute = withSearchRoute(SearchInfo);
+  const SearchPaginationWithSearchRoute = withSearchRoute(SearchPagination);
 
-    return (query.keyword)
-      ? store.dispatch(handleLoadItems({
-        keyword: decodeURI(keyword),
-        perPage,
-        startIndex,
-        filter,
-        orderBy,
-      }))
-      : store.dispatch(clearSearch());
-  }
-
-  render() {
-    const SearchInputWithSearchRoute = withSearchRoute(SearchInput);
-    const PerPageDropdownWithSearchRoute = withSearchRoute(PerPageDropdown);
-    const OrderByDropdownWithSearchRoute = withSearchRoute(OrderByDropdown);
-    const SearchInfoWithSearchRoute = withSearchRoute(SearchInfo);
-    const SearchPaginationWithSearchRoute = withSearchRoute(SearchPagination);
-
-    return (
-      <Layout page="Search">
-        <SearchInputWithSearchRoute />
-        <div css={css`
+  return (
+    <Layout page="Search">
+      <SearchInputWithSearchRoute />
+      <div css={css`
           display: flex;
           flex-direction: column;
           padding: 10px 0 20px;
@@ -53,8 +37,8 @@ class Search extends Component {
             padding-bottom: 10px;
           }
         `}>
-          <SearchRadioButtons />
-          <div css={css`
+        <SearchRadioButtons />
+        <div css={css`
             display: flex;
             padding-top: 10px;
             @media (min-width: 960px) {
@@ -62,7 +46,7 @@ class Search extends Component {
               margin-left: auto;
             }
          `}>
-            <div css={css`
+          <div css={css`
               flex: 1 1;
               margin-right: 10px;
               @media (min-width: 960px) {
@@ -70,19 +54,19 @@ class Search extends Component {
               }
             `}>
 
-              <PerPageDropdownWithSearchRoute />
-            </div>
-            <div css={css`
+            <PerPageDropdownWithSearchRoute />
+          </div>
+          <div css={css`
               flex: 1 1;
               @media (min-width: 960px) {
                 min-width:110px
               }
             `}>
-              <OrderByDropdownWithSearchRoute />
-            </div>
+            <OrderByDropdownWithSearchRoute />
           </div>
         </div>
-        <div css={css`
+      </div>
+      <div css={css`
           display: flex;
           flex-direction: column;
           padding: 10px 0 20px;
@@ -91,17 +75,31 @@ class Search extends Component {
             align-items: center;
           }
         `}>
-          <SearchInfoWithSearchRoute />
-          <div css={css`
+        <SearchInfoWithSearchRoute />
+        <div css={css`
           margin-left: auto;
         `}>
-            <SearchPaginationWithSearchRoute />
-          </div>
+          <SearchPaginationWithSearchRoute />
         </div>
-        <SearchResults />
-      </Layout>
-    );
-  }
+      </div>
+      <SearchResults />
+    </Layout>
+  );
 }
+
+Search.getInitialProps = async ({ store, req, res, query }) => {
+  const typeCastQuery = shallowParseInts(stateDefaults, query);
+  const { keyword, perPage, orderBy, filter, startIndex } = Object.assign(stateDefaults, typeCastQuery);
+
+  return (query.keyword)
+    ? store.dispatch(handleLoadItems({
+      keyword: decodeURI(keyword),
+      perPage,
+      startIndex,
+      filter,
+      orderBy,
+    }))
+    : store.dispatch(clearSearch());
+};
 
 export default Search;
